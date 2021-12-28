1f90985c216dab8d210b17fb471e5166427e756b
Christo1011 <44657162+Christo1011@users.noreply.github.com>
2021-12-28 14:47:12 -0500

    Add files via upload

---
+++ WorldTrotter4/WorldTrotter/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by christodoulos on 9/12/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+class MapViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewCController loaded its view. ")
+    }
+}
